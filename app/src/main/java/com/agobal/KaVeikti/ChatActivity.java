0967db8cca52bf4f9aafd612d444f6f6f2396926
GReeN-LT <mindaugas.laboga23@gmail.com>
2017-12-06 21:10:54 +0200

    toliau daryti

--- app/src/main/java/com/agobal/KaVeikti/ChatActivity.java
+++ app/src/main/java/com/agobal/KaVeikti/ChatActivity.java
@@ -2,22 +2,34 @@ package com.agobal.KaVeikti;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Window;
 
 public class ChatActivity extends AppCompatActivity {
 
     private String mChatUser;
 
+    private Toolbar mChatToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getWindow().requestFeature(Window.FEATURE_ACTION_BAR); // Removes app bar
-        super.onCreate(savedInstanceState);
-        getSupportActionBar().hide(); //  Removes app  bar also
 
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR); // Removes app bar
+     //   getSupportActionBar().hide(); //  Removes app  bar also
+        //new app bar
+     //   mChatToolbar = (Toolbar) findViewById(R.id.chat_app_bar);
+      // setSupportActionBar(mChatToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
+
 
         mChatUser = getIntent().getStringExtra("user_id");
 
+        getSupportActionBar().setTitle(mChatUser);
 
 
     }
